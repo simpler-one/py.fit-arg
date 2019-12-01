f07eb87dce3907753608dc3b1157f73875e78fb3
work-work-komei <40133262+work-work-komei@users.noreply.github.com>
2019-12-01 19:57:01 +0900

    Update argument_fitting.py

--- argument_fitting.py
+++ argument_fitting.py
@@ -1,8 +1,13 @@
 import inspect
 
 
-def ignore_longer():
-    pass
+def ignore_longer(func):
+    spec = inspect.getfullargspec(func)
+    arg_len = len(spec.args)
+    if hasattr(func, "__self__"):
+        arg_len -= 1
+
+    return lambda *args, **kwargs: 
 
 
 def ignore_unknown():
